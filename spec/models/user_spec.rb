fa5f8614b717fa5cde06509fd22889da5ffd95a1
matthewrpacker <matthewrpacker@gmail.com>
2016-10-31 11:46:06 -0600

    Add Links table - Add model spec validation for user and link relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
   it { should have_secure_password }
+  it { should have_many :links }
 end
