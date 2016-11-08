a3ae39b5a23f06bbee93926e61c215c246317c3a
matthewrpacker <matthewrpacker@gmail.com>
2016-11-08 09:31:50 -0700

    Remove ternary operator from link index into link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,8 @@ class Link < ApplicationRecord
   belongs_to :user
   validates :url, presence: true
   validates :url, url: true
+
+  def status
+    self.read ? "Mark as Unread" : "Mark as Read"
+  end
 end
