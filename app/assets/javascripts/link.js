5150607b0b3bb31d8725950d27d6155189fc7c7a
matthewrpacker <matthewrpacker@gmail.com>
2016-11-08 10:54:53 -0700

    Add status toggle on and off with refactor

--- app/assets/javascripts/link.js
+++ app/assets/javascripts/link.js
@@ -1,63 +1,29 @@
-$(function(){
-  $(".unread").on("click", ".change", function(event){
+$(function () {
+  $("button.toggle").on("click", function (event) {
     event.preventDefault();
+    var row = $(this).closest("tr")
+    var linkId = row.data("link-id")
 
-    link = $(this).closest('tr');
-    var id = $(link).attr('data-target');
-    var title = $(link).attr('data-title');
-    var url = $(link).attr('data-url');
-    var status = $(link).attr('data-status');
-    var type = $(link).attr('data-type');
-    var user = $(link).attr('data-user');
+    function toggleLink(row) {
+      row.toggleClass("read unread")
 
-    function toggleLink (row) {
-        $(row).hide()
-        var html = "<tr class='read' data-target='" + id
-        + "' data-type='" + type + "' data-title='" + title
-        + "' data-url='" + url + "' data-status='" + status
-        + "'><td><a href='" + url + "'>" + title
-        + "</a></td><td><a href='" + url + "'>" + url
-        + "</a></td><td>read</td><td><button type='button' name='button' class='change btn btn-primary btn-xs'>Mark as Unread</button></td><td><a class='btn btn-primary btn-xs' href='/users/"
-        + user + "/links/" + id + "/edit'>Edit Link</a></td></tr>"
-        $('tbody').append(html)
+      var newType = row.hasClass("read") ? "read" : "unread"
+      var oldType = row.hasClass("read") ? "unread" : "read"
+
+      var statusCells = row.children("td:nth-of-type(3)")
+      statusCells.text(newType)
+
+      var buttons = row.find("td > button")
+      buttons.text("Mark as " + $.camelCase(oldType))
     }
 
     $.ajax({
-      url: "/api/v1/links/" + id,
-      method: "PUT",
-      success: toggleLink(link),
-      error: console.log("Status update failed")
-    })
-  })
-
-  $(".read").on("click", ".change", function(event){
-    event.preventDefault();
-
-    link = $(this).closest('tr');
-    var id = $(link).attr('data-target');
-    var title = $(link).attr('data-title');
-    var url = $(link).attr('data-url');
-    var status = $(link).attr('data-status');
-    var type = $(link).attr('data-type');
-    var user = $(link).attr('data-user');
-
-    function toggleLink (row) {
-        $(row).hide()
-        var html = "<tr class='unread' data-target='" + id
-        + "' data-type='" + type + "' data-title='" + title
-        + "' data-url='" + url + "' data-status='" + status
-        + "'><td><a href='" + url + "'>" + title
-        + "</a></td><td><a href='" + url + "'>" + url
-        + "</a></td><td>unread</td><td><button type='button' name='button' class='change btn btn-primary btn-xs'>Mark as Read</button></td><td><a class='btn btn-primary btn-xs' href='/users/"
-        + user + "/links/" + id + "/edit'>Edit Link</a></td></tr>"
-        $('tbody').append(html)
-    }
-
-    $.ajax({
-      url: "/api/v1/links/" + id,
-      method: "PUT",
-      success: toggleLink(link),
-      error: console.log("Status update failed")
+      url: "/api/v1/links/" + linkId,
+      method: "PATCH",
+      success: toggleLink(row),
+      error: function (error) {
+        alert("Status update failed.")
+      }
     })
   })
 })
