7c469fad7f8ba8cce210600e46ad9f8b3ce7f4f4
matthewrpacker <matthewrpacker@gmail.com>
2016-11-08 11:58:35 -0700

    Add search bar functionality

--- app/assets/javascripts/link.js
+++ app/assets/javascripts/link.js
@@ -26,4 +26,26 @@ $(function () {
       }
     })
   })
+
+  $("#typed").keyup(function () {
+    var data = this.value.toUpperCase().split(" ")
+    var allRows = $("#searchBody").find("tr")
+
+    if (this.value == "") {
+      allRows.show()
+      return;
+    }
+    allRows.hide()
+
+    allRows.filter(function (index, value) {
+      var tr = $(this)
+
+      for (var i = 0; i < data.length; ++i) {
+        if (tr.text().toUpperCase().indexOf(data[i]) > -1) {
+          return true
+        }
+      }
+      return false
+    }).show()
+  })
 })
